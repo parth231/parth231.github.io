f49aeb8b65d7c16bc33d0066a2fdf9bdb5485f68
Parth santoki <59684868+parth231@users.noreply.github.com>
2021-02-17 13:42:31 +0530

    Add files via upload

---
+++ MUSICLY/js/slide.js
@@ -0,0 +1,10 @@
+function openNav(){
+      document.getElementById("mySidenav").style.width = "100%";
+      document.getElementById("main").style.visibility = "hidden";
+      document.getElementById("main").style.transitionDelay = "0s";
+}
+function closeNav(){
+      document.getElementById("mySidenav").style.width = "0";
+      document.getElementById("main").style.visibility = "visible";
+      document.getElementById("main").style.transitionDelay = "0.9s";
+}
